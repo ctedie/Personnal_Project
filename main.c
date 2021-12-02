7706921f958fcded2e32329e8f7d0dba8cf75223
Cédric TEDIE <cedric.tedie@slat.fr>
2021-12-02 10:02:41 +0100

    Mise en place du systick

--- main.c
+++ main.c
@@ -1,192 +1,131 @@
-//*****************************************************************************
-//
-// blinky.c - Simple example to blink the on-board LED.
-//
-// Copyright (c) 2013-2017 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-//
-// Texas Instruments (TI) is supplying this software for use solely and
-// exclusively on TI's microcontroller products. The software is owned by
-// TI and/or its suppliers, and is protected under applicable copyright
-// laws. You may not combine this software with "viral" open-source
-// software in order to form a larger program.
-//
-// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, FOR ANY REASON WHATSOEVER.
-//
-// This is part of revision 2.1.4.178 of the EK-TM4C1294XL Firmware Package.
-//
-//*****************************************************************************
+/*
+ * main.c
+ *
+ *  Created on: 24 nov. 2021
+ *      Author: Cedric
+ */
+
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "inc/hw_ints.h"
 #include "inc/hw_memmap.h"
 #include "driverlib/debug.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/pwm.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/systick.h"
 
-//*****************************************************************************
-//
-//! \addtogroup example_list
-//! <h1>Blinky (blinky)</h1>
-//!
-//! A very simple example that blinks the on-board LED using direct register
-//! access.
-//
-//*****************************************************************************
+//#include "rgb_led.h"
 
 
-#define RED_LED_PIN     GPIO_PIN_2
-#define GREEN_LED_PIN   GPIO_PIN_3
-#define BLUE_LED_PIN    GPIO_PIN_0
-#define RED_LED_PORT    GPIO_PORTF_BASE
-#define GREEN_LED_PORT  GPIO_PORTF_BASE
-#define BLUE_LED_PORT   GPIO_PORTG_BASE
+static uint8_t m_ucColor = 0;
 
-uint32_t ui32SysClock;
 
-//*****************************************************************************
-//
-// The error routine that is called if the driver library encounters an error.
-//
-//*****************************************************************************
-#ifdef DEBUG
-void
-__error__(char *pcFilename, uint32_t ui32Line)
+static uint32_t g_ui32SysClock;
+
+void Init_PWM(void);
+void Button_Interrupt(void);
+void LED_Init(void);
+void Init_Systick(void);
+
+int main(void)
 {
-    while(1);
-}
-#endif
+
+    int32_t val;
+    uint32_t ui32Loop;
+
+    g_ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
+                                             SYSCTL_OSC_MAIN |
+                                             SYSCTL_USE_PLL |
+                                             SYSCTL_CFG_VCO_480), 120000000);
 
 
-
-void Pwm_Led_Init(void);
-
-//*****************************************************************************
-//
-// Blink the on-board LED.
-//
-//*****************************************************************************
-int
-main(void)
-{
-    volatile uint32_t ui32Loop;
-
-    //
-    // Set clock
-    //
-    ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), 120000000);
+    //Init_PWM();
+    //LED_Init();
+    Init_Systick();
 
 
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
-
-    //
-    // Check if the peripheral access is enabled.
-    //
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))
     {
     }
+    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+//
+//    GPIOIntRegister(GPIO_PORTJ_BASE, Button_Interrupt);
+//
+//
+//    GPIOIntTypeSet(GPIO_PORTJ_BASE, GPIO_PIN_0, GPIO_FALLING_EDGE);
+//
+//    GPIOIntEnable(GPIO_PORTJ_BASE, GPIO_INT_PIN_0);
+//
+//    GPIOIntClear(GPIO_PORTJ_BASE, GPIO_INT_PIN_0);
+//
+//    IntEnable(INT_GPIOJ);
+//    IntMasterEnable();
+    //PWMGenConfigure(PWM0_BASE, )
 
-
-
-
-
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
-    {
-    }
-
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOG);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOG))
-    {
-    }
-
-
-
-
-
-
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
-
-    //
-    // Check if the peripheral access is enabled.
-    //
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))
-    {
-    }
-
-    //
-    // Enable the GPIO pin for the LED (PN0).  Set the direction as output, and
-    // enable the GPIO pin for digital function.
-    //
-    GPIOPinTypeGPIOOutput(RED_LED_PORT, RED_LED_PIN);
-    GPIOPinTypeGPIOOutput(GREEN_LED_PORT, GREEN_LED_PIN);
-    GPIOPinTypeGPIOOutput(BLUE_LED_PORT, BLUE_LED_PIN);
-
-    //
-    // Loop forever.
-    //
     while(1)
     {
         //
         // Turn on the LED.
         //
-        GPIOPinWrite(BLUE_LED_PORT, BLUE_LED_PIN, BLUE_LED_PIN);
+        //RGB_LED_SetState(BLUE, ON);
 
         //
         // Delay for a bit.
         //
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
-        {
-        }
-
+        SysCtlDelay(20000000);
         //
         // Turn off the LED.
         //
-        GPIOPinWrite(BLUE_LED_PORT, BLUE_LED_PIN, 0x0);
+        //RGB_LED_SetState(BLUE, OFF);
 
         //
         // Delay for a bit.
         //
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
-        {
-        }
+        SysCtlDelay(20000000);
+
+
+//        val = GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0);
     }
+
 }
 
-void Pwm_Led_Init(void)
+
+void Button_Interrupt(void)
 {
 
+    m_ucColor++;
+    if(m_ucColor >= 8)
+    {
+        m_ucColor = 0;
+    }
+
+    GPIOIntClear(GPIO_PORTJ_BASE, GPIO_INT_PIN_0);
+}
+
+void Init_PWM(void)
+{
     //
     // Enable the PWM0 peripheral
     //
     SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
 
-    //
+    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_2);
+
+    ///
     // Wait for the PWM0 module to be ready.
     //
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_PWM0))
     {
     }
-
     //
     // Configure the PWM generator for count down mode with immediate updates
     // to the parameters.
     //
-    PWMGenConfigure(PWM0_BASE, PWM_GEN_0,PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
+    //PWMGenConfigure(PWM0_BASE PWM_GEN_0, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
 
     //
     // Set the period. For a 50 KHz frequency, the period = 1/50,000, or 20
@@ -214,6 +153,48 @@ void Pwm_Led_Init(void)
     // Enable the outputs.
     //
     PWMOutputState(PWM0_BASE, (PWM_OUT_0_BIT | PWM_OUT_1_BIT), true);
+
 }
 
+void Init_Systick(void)
+{
+    uint32_t ui32Value;
+    //
+    // Configure and enable the SysTick counter.
+    //
+    SysTickPeriodSet(120000);
+    SysTickIntEnable();
+    SysTickEnable();
+    //
+    // Delay for some time...
+    //
+    //
+    // Read the current SysTick value.
+    //
+    ui32Value = SysTickValueGet();
 
+}
+
+static uint32_t ulCntTick = 0;
+bool bTest = false;
+void SystickISR(void)
+{
+
+    ulCntTick++;
+    if(ulCntTick >= 999)
+    {
+        ulCntTick = 0;
+        if(bTest)
+        {
+            GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0);
+            bTest = false;
+        }
+        else
+        {
+            GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
+            bTest = true;
+        }
+
+    }
+
+}
