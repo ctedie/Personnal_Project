09f014c1facea7826b9a92edddc2e35f0d27e172
Cedric <coolthug@hotmail.fr>
2021-12-17 18:15:27 +0100

    * Ajout des fichiers buttons.c et buttons.h

---
+++ MODULE/buttons/buttons.c
@@ -0,0 +1,23 @@
+/*
+ * buttons.c
+ *
+ *  Created on: 17 déc. 2021
+ *      Author: Cedric
+ */
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+
+#include "buttons.h"
+
+
+void BUTTONS_Init(void)
+{
+
+}
---
+++ MODULE/buttons/buttons.h
@@ -0,0 +1,13 @@
+/*
+ * buttons.h
+ *
+ *  Created on: 17 déc. 2021
+ *      Author: Cedric
+ */
+
+#ifndef MODULE_BUTTONS_BUTTONS_H_
+#define MODULE_BUTTONS_BUTTONS_H_
+
+
+
+#endif /* MODULE_BUTTONS_BUTTONS_H_ */
